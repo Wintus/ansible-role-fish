20346e7c9f6708df036919b97c5daa2f72cf5af9
wintus <6386129+wintus@users.noreply.github.com>
2020-06-19 23:11:39 +0900

    Remove broken aliases

--- files/fish/conf.d/aliases.fish
+++ files/fish/conf.d/aliases.fish
@@ -9,8 +9,6 @@ alias f='o .'
 alias e='emacsclient -cn'
 alias each='fmt -1'
 alias words='string split " "'
-alias prepend='string replace -r \'^\''
-alias append='string replace -r \'$\''
 alias tmpdir='cd (mktemp -d)'
 
 alias pbcopy='xsel -bi'
